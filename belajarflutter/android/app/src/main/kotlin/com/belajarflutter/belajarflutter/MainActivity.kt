d945ca8c62c854c2e3580f64e16f7609b420de04
NabiyaShevaIswandari <nabiyashevaiswandari17@gmail.com>
2024-02-28 22:47:58 +0800

    Belajar_Flutter

---
+++ belajarflutter/android/app/src/main/kotlin/com/belajarflutter/belajarflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.belajarflutter.belajarflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ belajarflutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:belajarflutter/column_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: ColumnWidget(),
+    );
+  }
+}
